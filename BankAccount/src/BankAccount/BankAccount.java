d29f19511f8e754cfadf95e19cf1f33b82bf1dd7
904154001 <904154001@L829WD150098.edu.cbe.ab.ca>
2021-02-23 14:10:58 -0700

    Bank account

---
+++ BankAccount/src/BankAccount/BankAccount.java
@@ -0,0 +1,16 @@
+package BankAccount;
+
+public class BankAccount {
+	
+	String accountName = "";
+	double balance = 0;
+	double withdrawalFee;
+	double annualInterestRate;
+	int accountID = 1000000;
+	
+	public BankAccount() {
+		
+		
+	}
+
+}
