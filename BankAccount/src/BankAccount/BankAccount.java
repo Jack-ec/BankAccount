dd4b1696e0ff622fc057f80013cccc60ebe877b0
904154001 <904154001@L829WD150098.edu.cbe.ab.ca>
2021-02-24 12:44:47 -0700

    accessors added still doesnt pass test

--- BankAccount/src/BankAccount/BankAccount.java
+++ BankAccount/src/BankAccount/BankAccount.java
@@ -1,13 +1,13 @@
 package BankAccount;
 
 public class BankAccount {
-	
+
 	String accountName = "";
 	double balance;
 	double withdrawalFee;
 	double annualInterestRate;
-	int accountID;
-	
+	static int accountID;
+	// constructors
 	public BankAccount() {
 		this("");
 	}
@@ -21,7 +21,29 @@ public class BankAccount {
 		this(accountName, balance, withdrawalFee, 0);
 	}
 	public BankAccount(String accountName, double balance, double withdrawalFee, double annualInterestRate) {
-		
+		this(accountName, balance, withdrawalFee, annualInterestRate, 0);
 	}
+	public BankAccount(String accountName, double balance, double withdrawalFee, double annualInteresrRate, int accountID) {
+
+	}
+	//getters
+	public String getAccountName() {
+		return this.accountName;
+	}
+	public double getBalance() {
+		return this.balance;
+	}
+	public double getWithdrawalFee() {
+		return this.withdrawalFee;
+	}
+	public double getAnnualInterestRate() {
+		return this.annualInterestRate;
+	}
+	public int getAccountID() {
+		return BankAccount.accountID;
+	}
+
+
+
 
 }
